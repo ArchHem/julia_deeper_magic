c63670be227bab2da589e747f7089928cbf245b2
ArchHem <84734676+ArchHem@users.noreply.github.com>
2024-07-29 00:17:34 +0200

    Finished Karger's Algo.

---
+++ las_vegas_monte_carlo/karger_algo.jl
@@ -0,0 +1,102 @@
+
+module karger
+
+#https://stanford-cs161.github.io/winter2021/assets/files/lecture16-notes.pdf
+using Graphs, DataStructures, Random, ProgressBars
+
+
+
+function simple_karger_optimized(X::Graph)
+
+    vert_num = nv(X)
+    num_components = vert_num
+    if vert_num == 2
+        return [1,2]
+    elseif vert_num <2
+        return zeros(Int64,vert_num)
+    else
+        #actual karger
+        supersets = DisjointSets{Int64}(1:vert_num)
+        for edg in shuffle(collect(edges(X)))
+            source = src(edg)
+            destination = dst(edg)
+            if !in_same_set(supersets, source, destination)
+                union!(supersets, source, destination)
+                num_components -= 1
+                if num_components <= 2
+                    break
+                end
+            end
+
+        end
+
+        return [(in_same_set(supersets, 1, v) ? 1 : 2) for v in 1:vert_num]
+    end
+    #convert return to readable format^
+end
+
+@inline function find_cut_num(X::Graph,V::Vector{Int64})
+    func(u) = V[src(u)] != V[dst(u)] #works on edges - yields 1 if the edges' ends are in a different superset
+    return count(func, edges(X))
+
+end
+
+function karger_full(X::Graph; N_tries::Int64 = ceil(Int64,binomial(nv(X),2)*log(nv(X))))
+    #a simple graph may have at most V(V-1)/2 cuts
+    min_num_cut = convert(Int64,nv(X)*(nv(X)-1)/2)
+    section = [0,0]
+    for _ in ProgressBar(1:N_tries)
+        res = simple_karger_optimized(X)
+        #count the number of edges that are 'cut' in the supersets
+        local_cut_num = find_cut_num(X,res)
+        if min_num_cut > local_cut_num
+            min_num_cut = local_cut_num
+            section = res
+        end
+
+    end
+    return min_num_cut, section
+end
+
+
+export simple_karger_optimized, karger_full
+#end module
+end
+
+using Graphs, .karger, GraphRecipes, Plots
+
+n1 = 10
+n2 = 8
+total_vertices = n1 + n2
+g = Graph(total_vertices)
+
+#there gotta be a better way tbh
+for u in 1:n1
+    for v in (u+1):n1
+        add_edge!(g, u, v)
+    end
+end
+
+
+for u in (n1+1):(n1+n2)
+    for v in (u+1):(n1+n2)
+        add_edge!(g, u, v)
+    end
+end
+
+
+add_edge!(g, 1, n1 + 1)
+
+G = erdos_renyi(30,90,seed = 1)
+
+M, S = karger_full(g)
+
+c1, c2 = :red, :blue
+
+colors = [S[i] == 1 ? c1 : c2 for i in eachindex(S)]
+
+
+z = plot(g,markercolor=colors, dpi = 1200, curves = false)
+
+
+
---
+++ las_vegas_monte_carlo/random_quicksort.jl
@@ -0,0 +1,152 @@
+module RSort
+
+using Random
+
+@inline function swap!(v, i::Int, j::Int)
+    #easier than pointer magic.
+    v[i], v[j] = v[j], v[i]
+    return Nothing
+end
+
+#https://rosettacode.org/wiki/Sorting_algorithms/Quicksort
+function quicksort_det!(X)
+
+    if length(X) > 1
+        pivot = X[1]
+        left, right = 1, length(X)
+        while left <= right
+            while X[left] < pivot
+                left += 1
+            end
+            while X[right] > pivot
+                right -= 1
+            end
+
+            if left <= right
+                swap!(X,left,right)
+                left += 1
+                right -= 1
+            end
+        end
+        #avoid copying via using @views
+        quicksort_det!(@views X[1:right])
+        quicksort_det!(@views X[left:end])
+    end
+    return X
+end
+
+function quicksort_ran!(X)
+
+    if length(X) > 1
+        pivot = X[rand((1:length(X)))]
+        left, right = 1, length(X)
+        while left <= right
+            while X[left] < pivot
+                left += 1
+            end
+            while X[right] > pivot
+                right -= 1
+            end
+
+            if left <= right
+                swap!(X,left,right)
+                left += 1
+                right -= 1
+            end
+        end
+        quicksort_det!(@views X[1:right])
+        quicksort_det!(@views X[left:end])
+    end
+    return X
+end
+
+export quicksort_det!, quicksort_ran!, swap!
+#end module
+end
+
+using BenchmarkTools, Plots
+
+#set up text cases
+
+#random cases
+
+using .RSort
+
+
+
+function compare_methods_randn(low::Int64 = 10,high::Int64 = 100,step::Int64=2)
+    iter = collect(low:step:high)
+
+    mtimes_det = zeros(length(iter))
+    mtimes_ran = zeros(length(iter))
+
+    for (index, N) in enumerate(iter)
+        
+        #dont interpolate!
+        results_det = (@benchmark quicksort_det!(randn($N)) )
+        results_ran = (@benchmark quicksort_ran!(randn($N)) )
+
+        mtimes_det[index] = mean(results_det.times::Vector{Float64})/1e9
+        mtimes_ran[index] = mean(results_ran.times::Vector{Float64})/1e9
+    end
+
+    return mtimes_det, mtimes_ran, iter
+end
+
+function gns(N::Int64; to_swap::Int64 = convert(Int64,floor(N/20)))
+    rn = collect(1:N)
+    for j in to_swap
+
+        swap!(rn,rand(1:N),rand(1:N))
+    end
+    return rn
+end
+
+function compare_methods_near_sorted(low::Int64 = 10,high::Int64 = 100,step::Int64=2)
+    iter = collect(low:step:high)
+
+    mtimes_det = zeros(length(iter))
+    mtimes_ran = zeros(length(iter))
+
+    for (index, N) in enumerate(iter)
+        
+        #dont interpolate!
+        results_det = (@benchmark quicksort_det!(gns($N)) )
+        results_ran = (@benchmark quicksort_ran!(gns($N)) )
+
+        mtimes_det[index] = mean(results_det.times::Vector{Float64})/1e9
+        mtimes_ran[index] = mean(results_ran.times::Vector{Float64})/1e9
+    end
+
+    return mtimes_det, mtimes_ran, iter
+end
+
+#change after prototyping!
+
+#=
+t1, t2, Ns = @time compare_methods_randn(2,100,4)
+
+p1 = plot(Ns,t1, xlabel = "Array size", 
+ylabel = "post-JIT execution time (s)", 
+title = "Quicksort!() on randn() arrays", grid = true,
+color =:red, label = "1-Pivot Quicksort", dpi = 1200)
+plot!(p1, Ns,t2,color =:green, label = "Random-Pivot Quicksort")
+=#
+
+t1, t2, Ns = @time compare_methods_near_sorted(2,100,4)
+
+p1 = plot(Ns,t1, xlabel = "Array size", 
+ylabel = "post-JIT execution time (s)", 
+title = "Quicksort!() on nearly-sorted arrays", grid = true,
+color =:red, label = "1-Pivot Quicksort", dpi = 1200)
+plot!(p1, Ns,t2,color =:green, label = "Random-Pivot Quicksort")
+
+
+
+
+
+
+
+
+
+
